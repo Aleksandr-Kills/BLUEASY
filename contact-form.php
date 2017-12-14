d733ae011c21a1904b9ba4f1d7ff35c42a55e59a
Jay <a>
2017-12-14 14:07:39 +0200

    part-six

---
+++ contact-form.php
@@ -0,0 +1,33 @@
+
+<?php
+
+$name = htmlspecialchars($_POST["name"]);
+$email = htmlspecialchars($_POST["email"]);
+$message = htmlspecialchars($_POST["message"]);
+$antispam = htmlspecialchars($_POST["antispam"]);
+
+$address = "salgari12345@gmail.com";
+$sub = "Сообщение с сайта ХХХ";
+
+$mes = "Сообщение с сайта ХХХ.\n
+Имя отправителя: $name 
+Электронный адрес отправителя: $email
+Текст сообщения:
+$message";
+
+if (empty($antispam))
+{
+    $from  = "From: $name <$email> \r\n Reply-To: $email \r\n";
+    if (mail($address, $sub, $mes, $from)) {
+        header('Refresh: 5; URL=http://');
+        echo '<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" /></head>
+    <body>Письмо отправлено, через 5 секунд вы вернетесь на страницу XXX</body>';}
+    else {
+        header('Refresh: 5; URL=http://biznessystem.ru');
+        echo '<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" /></head>
+    <body>Письмо не отправлено, через 5 секунд вы вернетесь на страницу YYY</body>';}
+}
+exit;
+?>
